610c5474acdf2fb9d239025869365d02d9e7749d
Shubert <103430520+Sk0voroda@users.noreply.github.com>
2024-08-28 21:08:46 +0300

    added auth

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,11 @@
 import { Outlet } from "react-router-dom";
+import { useAuth } from "./context/AuthProvider";
 
 export const Layout = () => {
+  const { user } = useAuth();
+
+  console.log(user);
+
   return (
     <>
       <div>header</div>
